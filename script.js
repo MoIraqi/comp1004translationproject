b8269caf1d400eb1cbc9804a440ea41b65e468a4
Mohammad Iraqi <146234473+MoIraqi@users.noreply.github.com>
2024-02-26 16:43:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let translationData;
 
+function handleFile() {
+    const fileInput = document.getElementById('json-input');
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        try {
+            translationData = JSON.parse(event.target.result);
+            console.log('File successfully loaded.');
+        } catch (error) {
+            console.error('Error parsing file:', error);
+        }
+    };
+
+    reader.readAsText(file);
+}
+
+function translateWord() {
+    const wordInput = document.getElementById("word-input");
+    const translationOutput = document.getElementById("translation-output");
+
+    const word = wordInput.value.trim(); // Trim the input to remove leading/trailing spaces
+
+    if (!translationData) {
+        translationOutput.textContent = 'Please load a JSON file first.';
+        return;
+    }
+
+    // Check if the word exists in the loaded JSON data
+    if (translationData[word]) {
+        translationOutput.textContent = translationData[word];
+    } else {
+        translationOutput.textContent = "Translation not found for the word: " + word;
+    }
+}
